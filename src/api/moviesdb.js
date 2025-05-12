b7476860ba21ccb19c245543ad2e1294751fab56
Omar Hamdy <omarhamdy123987@gmail.com>
2025-05-12 11:23:25 +0300

    refactor apis code

--- src/api/moviesdb.js
+++ src/api/moviesdb.js
@@ -8,11 +8,19 @@ const api = axios.create({
   },
 });
 
-const endpoints = {
-  trending: "trending/movie/day",
-  upcoming: "movie/upcoming",
-  topRated: "movie/top_rated",
-};
+// static endpoints
+const trendingMoviesEndpoint = "trending/movie/day";
+const upcomingMoviesEndpoint = "movie/upcoming";
+const topRatedMoviesEndpoint = "movie/top_rated";
+
+// dynamic endpoints
+const movieDetailsEndpoint = (id) => `movie/${id}`;
+const similarMoviesEndpoint = (id) => `movie/${id}/similar`;
+const searchMoviesEndpoint = (query) => `search/movie?query=${query}`;
+
+const movieCreditsEndpoint = (id) => `movie/${id}/credits`;
+const personDetailsEndpoint = (id) => `person/${id}`;
+const personMoviesEndpoint = (id) => `person/${id}/movie_credits`;
 
 export const imageOriginal = (path) =>
   path ? `https://image.tmdb.org/t/p/w200/${path}` : null;
@@ -32,44 +40,56 @@ async function apiCall(endpoint, errorMessage) {
 }
 
 export async function fetchTrendingMovies() {
-  return await apiCall(endpoints.trending, "Error at fetching trending movies");
+  return await apiCall(
+    trendingMoviesEndpoint,
+    "Error at fetching trending movies"
+  );
 }
 export async function fetchUpcomingMovies() {
-  return await apiCall(endpoints.upcoming, "Error at fetching upcoming movies");
+  return await apiCall(
+    upcomingMoviesEndpoint,
+    "Error at fetching upcoming movies"
+  );
 }
 export async function fetchTopRatedMovies() {
   return await apiCall(
-    endpoints.topRated,
+    topRatedMoviesEndpoint,
     "Error at fetching top rated movies"
   );
 }
 export async function fetchMovieDetails(id) {
-  return await apiCall(`movie/${id}`, "Error at fetching movie details");
+  return await apiCall(
+    movieDetailsEndpoint(id),
+    "Error at fetching movie details"
+  );
 }
 export async function fetchSimilarMovies(id) {
   return await apiCall(
-    `movie/${id}/similar`,
+    similarMoviesEndpoint(id),
     "Error at fetching similar movies"
   );
 }
 export async function fetchMovieCredits(id) {
   return await apiCall(
-    `movie/${id}/credits`,
+    movieCreditsEndpoint(id),
     "Error at fetching movies detials"
   );
 }
 export async function fetchPersonDetails(id) {
-  return await apiCall(`person/${id}`, "Error at fetching person detials");
+  return await apiCall(
+    personDetailsEndpoint(id),
+    "Error at fetching person detials"
+  );
 }
 export async function fetchPersonMovies(id) {
   return await apiCall(
-    `person/${id}/movie_credits`,
+    personMoviesEndpoint(id),
     "Error at fetching person movies"
   );
 }
 export async function fetchSearchMovies(query) {
   return await apiCall(
-    `search/movie?query=${query}`,
+    searchMoviesEndpoint(query),
     "Error at fetching search movies"
   );
 }
